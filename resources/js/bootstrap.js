d80d9746fd7699aa9f855eb8357e96bc8c3e63bd
osama-mustafa <semsem6661@gmail.com>
2022-06-17 23:00:26 +0200

    Fix setting of laravel-echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,8 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
